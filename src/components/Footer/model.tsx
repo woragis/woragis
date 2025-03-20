46cdf28d3072efe1b6732a06c1c7839ca83d9865
woragis <woragis@users.noreply.github.com>
2025-03-20 04:45:54 +0000

    deploy: bc765cd38bd1f07a67245be20113c7cde1235ce7

--- src/components/Footer/model.tsx
+++
@@ -1,3 +0,0 @@
-export const footerModel = () => {
-  return {}
-}
--- src/components/Navbar/model.tsx
+++
@@ -1,9 +0,0 @@
-export const navbarModel = () => {
-  const links = [
-    { name: 'projects', path: '#projects' },
-    { name: 'about', path: '#about' },
-    { name: 'certifications', path: '#certifications' },
-  ]
-
-  return { links }
-}
