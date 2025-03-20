52f9f1e630f444cb8d3cf1955f74b9989edd776f
woragis <masteringthecode.woragis+github@gmail.com>
2025-03-19 21:45:35 -0300

    Create global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react'
+
+export const styles = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    scroll-behavior: smooth;
+  }
+`
