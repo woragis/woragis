dffa01eecfa8a538384beec6470320d391c74c2f
woragis <masteringthecode.woragis+github@gmail.com>
2025-03-19 22:59:20 -0300

    Create typewriter effect with javascript

--- src/components/HeroSection/model.tsx
+++ src/components/HeroSection/model.tsx
@@ -1,3 +1,35 @@
 export const heroSectionModel = () => {
-  return {}
+  const texts = [
+    // 'Hello, my name is Jezreel',
+    // 'but you can call me jazz',
+    'Jezreel de Andrade',
+    'woragis',
+  ]
+
+  function incrementString(word: string) {}
+  function decrementFromString(size: number) {}
+
+  async function textTypingEffect(
+    element: HTMLElement,
+    text: string,
+    i: number = 0
+  ) {
+    element.textContent += text[i]
+
+    if (i === text.length - 1) return
+
+    setTimeout(() => textTypingEffect(element, text, i + 1), 50)
+  }
+  async function textDeletionEffect(element: HTMLElement) {
+    const currentText = element.textContent || ''
+    const currentLength = currentText.length || 0
+    if (currentLength === 0) return
+
+    const newText = currentText.slice(0, currentLength - 1)
+    element.textContent = newText
+
+    setTimeout(() => textDeletionEffect(element), 50)
+  }
+
+  return { textTypingEffect, texts, textDeletionEffect }
 }
--- src/components/HeroSection/view.tsx
+++ src/components/HeroSection/view.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useRef } from 'react'
 import { Hero, Names, Side, TemporaryName, TypewriterP } from './styles'
+import { heroSectionModel } from './model'
 
-export const HeroSectionView = () => {
+export const HeroSectionView = ({
+  textTypingEffect,
+  textDeletionEffect,
+  texts,
+}: ReturnType<typeof heroSectionModel>) => {
+  const namesDiv = useRef<HTMLParagraphElement>(null)
+
+  const namesEffect = async () => {
+    if (namesDiv.current) {
+      textTypingEffect(namesDiv.current, texts[0]).then(() => {
+        // if (namesDiv.current) textDeletionEffect(namesDiv.current)
+      })
+    }
+  }
+  useEffect(() => {
+    // Type my name
+
+    // Type 'or'
+    // Type woragis
+    // Delete everything
+    // restart animation
+    namesEffect()
+  }, [])
+
   return (
     <Hero>
       <Side>
         <span>div</span>
         <p>picture of me</p>
+        <p ref={namesDiv}></p>
       </Side>
       <Side>
         <Names>
