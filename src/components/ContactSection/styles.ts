4cc9a385a29e2bf29a24f89aff50d6da98897776
woragis <masteringthecode.woragis+github@gmail.com>
2025-03-21 15:19:03 -0300

    Update Contact section styles
    Set background color
    Set beatiful styles for form and its title
    Set a funny side picture

---
+++ src/components/ContactSection/styles.ts
@@ -0,0 +1,134 @@
+import styled from '@emotion/styled'
+
+export const Section = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  justify-content: space-evenly;
+  align-items: center;
+  min-height: 500px;
+  background-color: var(--bg-primary);
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  gap: 20px;
+  width: max-content;
+  margin: 40px auto;
+  padding: 20px;
+`
+
+export const Title = styled.h2`
+  text-align: left;
+  display: inline-block;
+  position: relative;
+  color: var(--accent-secondary);
+  font-weight: 600;
+  font-size: 40px;
+  margin-bottom: 25px;
+  position: relative;
+  z-index: 2;
+  color: transparent;
+
+  background: linear-gradient(
+    270deg,
+    var(--accent-primary),
+    var(--accent-secondary)
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+  &::after {
+    content: '';
+    background-color: var(--accent-secondary);
+    width: 120px;
+    height: 5px;
+    border-radius: 10px;
+    position: absolute;
+    bottom: -10px;
+    left: 0;
+  }
+`
+
+export const Field = styled.input`
+  width: 400px;
+  height: 50px;
+  border: 2px solid var(--bg-secondary);
+  outline: none;
+  padding-inline: 25px;
+  font-weight: 500;
+  color: var(--text-primary);
+  background-color: var(--bg-secondary);
+  border-radius: 50px;
+  font-size: 16px;
+  transition: 300ms;
+
+  &:focus {
+    border-color: var(--accent-primary);
+  }
+  &::placeholder {
+    color: var(--text-muted);
+  }
+
+  @media (max-width: 800px) {
+    & {
+      width: 800vw;
+    }
+  }
+`
+
+export const Message = styled.textarea`
+  height: 140px;
+  padding-top: 15px;
+  border-radius: 20px;
+  width: 400px;
+  border: 2px solid var(--bg-secondary);
+  outline: none;
+  padding-inline: 25px;
+  font-weight: 500;
+  color: var(--text-primary);
+  background-color: var(--bg-secondary);
+  font-size: 16px;
+  transition: 300ms;
+  resize: none;
+
+  &:focus {
+    border-color: var(--accent-primary);
+  }
+  &::placeholder {
+    color: var(--text-muted);
+  }
+`
+
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px 30px;
+  font-size: 16px;
+  color: var(--text-primary);
+  gap: 10px;
+  border: none;
+  border-radius: 50px;
+  background: linear-gradient(230deg, var(--accent-primary), var(--bg-primary));
+  cursor: pointer;
+  min-width: 40%;
+
+  img {
+    height: 15px;
+  }
+`
+
+export const SectionImg = styled.img`
+  width: 300px;
+  border-radius: 30px;
+  margin-inline: auto;
+  @media (max-width: 800px) {
+    & {
+      display: none;
+    }
+  }
+`
