f275922e2d4685fe52b15318746742a05309f50c
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-10 02:01:57 -0300

    Update react outputs

--- terraform/react/outputs.tf
+++ terraform/react/outputs.tf
@@ -1,14 +1,14 @@
 output "static_site_bucket_name" {
-  value       = aws_s3_bucket.static_site.bucket
+  value       = module.react.static_site_bucket_name
   description = "Name of the S3 bucket hosting the static site"
 }
 
 output "logging_bucket_name" {
-  value       = aws_s3_bucket.logging_bucket.bucket
+  value       = module.react.logging_bucket_name
   description = "Name of the S3 bucket for logging"
 }
 
 output "website_endpoint" {
-  value = aws_s3_bucket_website_configuration.site.website_endpoint
+  value       = module.react.website_endpoint
   description = "S3 website endpoint"
 }
