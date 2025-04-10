85595e96d3ecf621442ce45ffc0d9db8168d2091
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-10 01:51:42 -0300

    Update terraform/cdn variables

--- terraform/cdn/main.tf
+++ terraform/cdn/main.tf
@@ -4,15 +4,9 @@ provider "aws" {
 
 module "cdn" {
   source                       = "git::https://github.com/woragis/terraform-cdn.git//?ref=v1"
-  project_name                 = var.project_name
-  terraform_dir                = "./"
-  aws_region                   = var.aws_region
-  root_domain                  = var.root_domain
-  subdomain                    = var.subdomain
-  environment                  = var.environment
   origin_domain_name           = var.origin_domain_name
-  logging_bucket_domain_name   = var.logging_bucket_domain_name
-  cloudfront_distribution_name = var.cloudfront_distribution_name
-  ssl_minimum_protocol_version = var.ssl_minimum_protocol_version
   tags                         = var.tags
+  full_domain                  = "${var.subdomain}.${var.root_domain}"
+  environment                  = var.environment
+  ssl_minimum_protocol_version = var.ssl_minimum_protocol_version
 }
