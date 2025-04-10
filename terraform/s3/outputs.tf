93e3767aa06e6a7038dea71bec668630b255d440
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-10 02:21:04 -0300

    Update terraform s3 action

--- terraform/s3/outputs.tf
+++ terraform/s3/outputs.tf
@@ -1,5 +1,5 @@
-output "static_site_bucket_name" {
-  value       = module.react.static_site_bucket_name
+output "s3_bucket_name" {
+  value       = module.react.s3_bucket_name
   description = "Name of the S3 bucket hosting the static site"
 }
 
