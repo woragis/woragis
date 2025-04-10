31acbfb86a3804f46234473c8c2f479db208520c
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-09 21:14:06 -0300

    Update terraform deploy action variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 module "react_app" {
   source = "git::https://github.com/woragis/terraform-react.git//?ref=v1"
 
-  project_name = "WoragisWebsite"
-  root_domain = "woragis.com"
-  subdomain = "www"
-  aws_region = "us-east-1"
+  # project_name = "WoragisWebsite"
+  # root_domain = "woragis.com"
+  # subdomain = "www"
+  # aws_region = "us-east-1"
 }
