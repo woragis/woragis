a2497bda95fafde8f61de6e6cd5ad1f5e542085c
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-10 01:52:48 -0300

    Update route53 variables

--- terraform/route53/main.tf
+++ terraform/route53/main.tf
@@ -4,11 +4,4 @@ provider "aws" {
 
 module "route53" {
   source             = "git::https://github.com/woragis/terraform_route53.git//?ref=v1"
-  project_name       = var.project_name
-  terraform_dir      = "./"
-  aws_region         = var.aws_region
-  root_domain        = var.root_domain
-  subdomain          = var.subdomain
-  target_domain      = var.target_domain
-  target_record_type = var.target_record_type
 }
