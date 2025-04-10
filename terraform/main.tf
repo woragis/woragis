ac60863097a6f60b999e0a54ed0187eff4914be0
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-09 21:33:08 -0300

    Update default variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,4 +5,10 @@ module "react_app" {
   root_domain  = var.root_domain
   environment  = var.environment
   aws_region   = var.aws_region
+
+  # Optional variables with fallback logic
+  s3_bucket_name              = ""
+  logging_bucket_name         = ""
+  cloudfront_distribution_name = ""
+  route53_zone_id             = "" # optional but keep it clean
 }
