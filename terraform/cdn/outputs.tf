107b9d48a3b71bc045263331f8a5ef8109144078
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-11 16:26:57 -0300

    Update cdn outputs

--- terraform/cdn/outputs.tf
+++ terraform/cdn/outputs.tf
@@ -1,9 +1,19 @@
 output "cloudfront_domain_name" {
-  value       = aws_cloudfront_distribution.this.domain_name
   description = "Domain name of the CloudFront distribution"
+  value       = module.cdn.cloudfront_domain_name
+}
+
+output "full_domain" {
+  description = "The full domain name (subdomain + root)"
+  value       = module.cdn.full_domain
+}
+
+output "zone_id" {
+  description = "Route53 zone ID (from primary zone data source)"
+  value       = module.cdn.zone_id
 }
 
 output "cloudfront_distribution_id" {
-  value       = aws_cloudfront_distribution.this.id
   description = "ID of the CloudFront distribution"
+  value       = module.cdn.cloudfront_distribution_id
 }
