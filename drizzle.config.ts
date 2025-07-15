8cdcc0b9b221b57590615a9d5320044ece0dd1f2
Jezreel de Andrade <79119700+woragis@users.noreply.github.com>
2025-07-15 15:50:00 +0000

    Update drizzle.config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
 export default defineConfig({
-	schema: './src/lib/server/db/schema.ts',
-	dialect: 'sqlite',
-	dbCredentials: { url: process.env.DATABASE_URL },
-	verbose: true,
-	strict: true
+  schema: './src/lib/server/db/schema.ts',
+  out: './drizzle',
+  // driver: 'better-sqlite3',
+  dialect: 'sqlite',
+  dbCredentials: {
+    url: './db/dev.db'
+  },
+  verbose: true,
+  strict: true
 });
