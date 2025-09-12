e4d538c8b4822b8f88aef627f0f4de3c4c2d61d6
woragis <masteringthecode.woragis+github@gmail.com>
2025-09-12 15:13:29 -0300

    Create language switcher

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,3 +2,5 @@ export { Button } from "./Button";
 export { Card } from "./Card";
 export { Section } from "./Section";
 export { Container } from "./Container";
+export { ThemeToggle } from "./ThemeToggle";
+export { LanguageSwitcher } from "./LanguageSwitcher";
