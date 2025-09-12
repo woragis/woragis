e74204e312c9ddef6f3d3f40761a534f6630d505
woragis <masteringthecode.woragis+github@gmail.com>
2025-09-12 20:17:10 -0300

    Fix drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./src/lib/db/schema.ts",
+  schema: "./src/server/db/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
