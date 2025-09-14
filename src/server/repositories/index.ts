d34d014ba090f25d661c71f978777816daa048ec
woragis <masteringthecode.woragis+github@gmail.com>
2025-09-14 18:16:45 -0300

    Delete old files

--- src/server/repositories/index.ts
+++ src/server/repositories/index.ts
@@ -1,37 +1,65 @@
 // Import all repositories
 import { AuthRepository } from "./auth.repository";
 import { ProjectRepository } from "./project.repository";
-import { TagRepository } from "./tag.repository";
-import { CategoryRepository } from "./category.repository";
-import { LanguageRepository } from "./language.repository";
 import { FrameworkRepository } from "./framework.repository";
 import { SettingsRepository } from "./settings.repository";
 import { TestimonialRepository } from "./testimonial.repository";
 import { BlogRepository } from "./blog.repository";
 import { ExperienceRepository } from "./experience.repository";
+import { BlogTagRepository } from "./blog-tag.repository";
+import { ProjectTagRepository } from "./project-tag.repository";
+import { TestimonialTagRepository } from "./testimonial-tag.repository";
+import {
+  AboutCoreRepository,
+  MusicGenreRepository,
+  LastListenedSongRepository,
+  AnimeRepository,
+  BookRepository,
+  PoliticalViewRepository,
+  YoutuberRepository,
+  GameRepository,
+} from "./about";
 
 // Export all repositories
 export {
   AuthRepository,
   ProjectRepository,
-  TagRepository,
-  CategoryRepository,
-  LanguageRepository,
   FrameworkRepository,
   SettingsRepository,
   TestimonialRepository,
   BlogRepository,
   ExperienceRepository,
+  BlogTagRepository,
+  ProjectTagRepository,
+  TestimonialTagRepository,
+  AboutCoreRepository,
+  MusicGenreRepository,
+  LastListenedSongRepository,
+  AnimeRepository,
+  BookRepository,
+  PoliticalViewRepository,
+  YoutuberRepository,
+  GameRepository,
 };
 
 // Create singleton instances
 export const authRepository = new AuthRepository();
 export const projectRepository = new ProjectRepository();
-export const tagRepository = new TagRepository();
-export const categoryRepository = new CategoryRepository();
-export const languageRepository = new LanguageRepository();
 export const frameworkRepository = new FrameworkRepository();
 export const settingsRepository = new SettingsRepository();
 export const testimonialRepository = new TestimonialRepository();
 export const blogRepository = new BlogRepository();
 export const experienceRepository = new ExperienceRepository();
+export const blogTagRepository = new BlogTagRepository();
+export const projectTagRepository = new ProjectTagRepository();
+export const testimonialTagRepository = new TestimonialTagRepository();
+
+// Create about repository instances
+export const aboutCoreRepository = new AboutCoreRepository();
+export const musicGenreRepository = new MusicGenreRepository();
+export const lastListenedSongRepository = new LastListenedSongRepository();
+export const animeRepository = new AnimeRepository();
+export const bookRepository = new BookRepository();
+export const politicalViewRepository = new PoliticalViewRepository();
+export const youtuberRepository = new YoutuberRepository();
+export const gameRepository = new GameRepository();
