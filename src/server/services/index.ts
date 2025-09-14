94ec69dba1f85aae95258c02008b668b54568f09
woragis <masteringthecode.woragis+github@gmail.com>
2025-09-14 18:17:03 -0300

    Delete old files

--- src/server/services/index.ts
+++ src/server/services/index.ts
@@ -1,37 +1,50 @@
 // Import all services
 import { AuthService } from "./auth.service";
 import { ProjectService } from "./project.service";
-import { CategoryService } from "./category.service";
-import { TagService } from "./tag.service";
-import { LanguageService } from "./language.service";
 import { FrameworkService } from "./framework.service";
 import { SettingsService } from "./settings.service";
 import { TestimonialService } from "./testimonial.service";
 import { BlogService } from "./blog.service";
 import { ExperienceService } from "./experience.service";
+import { BlogTagService } from "./blog-tag.service";
+import { ProjectTagService } from "./project-tag.service";
+import { TestimonialTagService } from "./testimonial-tag.service";
+import {
+  AboutCoreService,
+  MusicGenreService,
+  LastListenedSongService,
+} from "./about";
 
 // Export all services
 export {
   AuthService,
   ProjectService,
-  CategoryService,
-  TagService,
-  LanguageService,
   FrameworkService,
   SettingsService,
   TestimonialService,
   BlogService,
   ExperienceService,
+  BlogTagService,
+  ProjectTagService,
+  TestimonialTagService,
+  AboutCoreService,
+  MusicGenreService,
+  LastListenedSongService,
 };
 
 // Create singleton instances
 export const authService = new AuthService();
 export const projectService = new ProjectService();
-export const categoryService = new CategoryService();
-export const tagService = new TagService();
-export const languageService = new LanguageService();
 export const frameworkService = new FrameworkService();
 export const settingsService = new SettingsService();
 export const testimonialService = new TestimonialService();
 export const blogService = new BlogService();
 export const experienceService = new ExperienceService();
+export const blogTagService = new BlogTagService();
+export const projectTagService = new ProjectTagService();
+export const testimonialTagService = new TestimonialTagService();
+
+// Create about service instances
+export const aboutCoreService = new AboutCoreService();
+export const musicGenreService = new MusicGenreService();
+export const lastListenedSongService = new LastListenedSongService();
