070da328e57d5fd051d2c5618fafd8aaf0606836
woragis <masteringthecode.woragis+github@gmail.com>
2025-09-28 03:47:39 -0300

    Update ui components

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Search } from "lucide-react";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils/utils";
 
 interface SearchBarProps {
   value: string;
