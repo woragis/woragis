c0df859be398b39836ac42e2fbcad99ea35378d5
woragis <masteringthecode.woragis+github@gmail.com>
2025-09-28 03:56:09 -0300

    Update query client provider

--- src/components/QueryClientProvider.tsx
+++ src/components/QueryClientProvider.tsx
@@ -1,22 +1,10 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { useState } from "react";
 
 export function QueryClientProviderWrapper({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 60 * 1000, // 1 minute
-            refetchOnWindowFocus: false,
-          },
-        },
-      })
-  );
-
   return (
     <QueryClientProvider client={queryClient}>
       {children}
