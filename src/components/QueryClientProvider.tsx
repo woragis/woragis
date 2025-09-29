c6dc490263ac7d22f3643d0d7943cdebd2de247a
woragis <masteringthecode.woragis+github@gmail.com>
2025-09-28 23:33:32 -0300

    Update query client provider

--- src/components/QueryClientProvider.tsx
+++ src/components/QueryClientProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { queryClient } from "@/lib";
+import { queryClient } from "@/lib/clients/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
