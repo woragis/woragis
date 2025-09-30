26b0d1721e3aed4ada0442b4b8e8ea0e12bfa197
woragis <masteringthecode.woragis+github@gmail.com>
2025-09-30 01:41:31 -0300

    Create money domain admin pages components

---
+++ src/components/pages/admin/money/index.ts
@@ -0,0 +1,4 @@
+export { IdeaForm } from "./IdeaForm";
+export { IdeaNodeForm } from "./IdeaNodeForm";
+export { IdeaCanvas } from "./IdeaCanvas";
+export { AiChatInterface } from "./AiChatInterface";
