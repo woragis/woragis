8b4f9863b542deeb7b15de6e49f4ddedb0bb5142
woragis <masteringthecode.woragis+github@gmail.com>
2025-10-03 22:14:17 -0300

    Create app.dart

---
+++ mobile/lib/app/app.dart
@@ -0,0 +1,6 @@
+// App exports
+export 'config/app_config.dart';
+export 'pages/home_page.dart';
+export 'pages/create_page.dart';
+export 'pages/main_navigation_page.dart';
+export 'router/app_router.dart';
